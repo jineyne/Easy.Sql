f0ef1cbb76f02603119bce7af43cdc54ef4bc041
jineyne <jineyne@gmail.com>
2020-02-03 23:24:44 +0900

    feat: Add projects to test with sqlite

---
+++ Easy.Sql.SQLite.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.Sql.SQLite.Test {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
