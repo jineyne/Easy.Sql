ef2e1299dd116f03dea4f4c30091aa7b8467a101
jineyne <jineyne@gmail.com>
2020-02-03 23:15:08 +0900

    feat: Add Importer for MEF

---
+++ Easy.Sql/Importer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Easy.Sql {
+    public class Importer {
+        private CompositionContainer mContainer;
+
+        public int AvailableNumberOfOperation => mContainer?.Catalog.Count() ?? 0;
+
+        public void Initialize() {
+            var catalog = new AggregateCatalog(
+                new DirectoryCatalog(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)));
+            mContainer = new CompositionContainer(catalog);
+
+            var batch = new CompositionBatch();
+            batch.AddExportedValue(mContainer);
+
+            mContainer.Compose(batch);
+        }
+
+        public object GetInstance(Type serviceType, String key) {
+            string contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = mContainer.GetExportedValues<object>(contract);
+
+            if (exports.Any())
+                return exports.First();
+
+            throw new Exception($"Could not locate any instances of contract {contract}.");
+        }
+
+        protected IEnumerable<object> GetAllInstances(Type serviceType) {
+            return mContainer.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+        }
+
+        protected void BuildUp(object instance) {
+            mContainer.SatisfyImportsOnce(instance);
+        }
+    }
+}
