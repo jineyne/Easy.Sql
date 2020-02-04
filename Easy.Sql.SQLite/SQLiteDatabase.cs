f143742386398fafc2c7daf627e9b2ead44463d2
jineyne <jineyne@gmail.com>
2020-02-04 21:45:18 +0900

    feat: Update EasyDatabase to add execute query

--- Easy.Sql.SQLite/SQLiteDatabase.cs
+++ Easy.Sql.SQLite/SQLiteDatabase.cs
@@ -1,13 +1,48 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Data;
 using System.Data.SQLite;
 
 namespace Easy.Sql.SQLite {
     [Export(typeof(IEasyDatabase))]
     public class SQLiteDatabase : IEasyDatabase {
         private SQLiteConnection mConn;
+
         public void Open(string fileName) {
             mConn = new SQLiteConnection($"Data Source={fileName};Version=3;Pooling=False");
             mConn.Open();
+        }
+
+        public DataTable Execute(string query) {
+            var result = new DataTable();
+
+            using (var cmd = new SQLiteCommand(query, mConn)) {
+                using (var rdr = cmd.ExecuteReader()) {
+                    result.Load(rdr);
+                    return result;
+                }
+            }
+        }
+
+        public DataTable ExecuteSafe(string query) {
+            var result = new DataTable();
+
+            try {
+                using (var cmd = new SQLiteCommand(query, mConn)) {
+                    using (var rdr = cmd.ExecuteReader()) {
+                        result.Load(rdr);
+                        return result;
+                    }
+                }
+            } catch (Exception) { }
+
+            return result;
+        }
+
+        public object ExecuteScalar(string query) {
+            using (var cmd = new SQLiteCommand(query, mConn)) {
+                return cmd.ExecuteScalar();
+            }
         }
     }
 }
--- Easy.Sql/IEasyDatabase.cs
+++ Easy.Sql/IEasyDatabase.cs
@@ -1,5 +1,20 @@
-﻿namespace Easy.Sql {
-    public interface IDatabase {
+﻿using System.Data;
+
+namespace Easy.Sql {
+    public interface IEasyDatabase {
         void Open(string fileName);
+
+        /// <summary>
+        ///     Execute query.
+        /// </summary>
+        DataTable Execute(string query);
+
+        /// <summary>
+        ///     Execute query.
+        ///     This method contain try - catch statement
+        /// </summary>
+        DataTable ExecuteSafe(string query);
+
+        object ExecuteScalar(string query);
     }
 }
